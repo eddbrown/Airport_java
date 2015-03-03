6bde805c4a56f93a5b66ac5c9c54eb888ddf0678
Edward Brown <edward.brown.london@gmail.com>
2015-03-03 17:05:51 +0000

    Plane can land and take off

---
+++ spec/PlaneSpec.js
@@ -0,0 +1,21 @@
+describe('Plane', function(){
+  var plane;
+
+  it('is flying when created', function(){
+    plane = new Plane; 
+    expect(plane.isFlying).toBe(true);
+  });
+
+  it('can land', function(){
+    plane = new Plane();
+    plane.land();
+    expect(plane.isFlying).toBe(false);
+  });
+
+  it('take off', function(){
+    plane = new Plane();
+    plane.land();
+    plane.takeOff();
+    expect(plane.isFlying).toBe(true)
+  });
+});
